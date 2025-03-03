1d67f6d36918db32329f4b7f89c6686c20d84f5e
Isuru Udakara <pereraisuruudakara@gmail.com>
2025-03-03 22:29:42 +0530

    Set size to the window

--- MyFrame.java
+++ MyFrame.java
@@ -5,6 +5,7 @@ public class MyFrame extends JFrame{
     MyFrame(){
         this.setTitle("My Frame");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(600, 500);
     }
 
     public static void main(String[] params){
